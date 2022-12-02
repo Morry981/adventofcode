3e0c0a9a664655c828da367f14478343ce6e2b90
Morry98 <89313187+Morry981@users.noreply.github.com>
2022-12-02 08:28:37 +0000

    Pending changes exported from your codespace

---
+++ advent-1/index.php
@@ -0,0 +1,32 @@
+<?php
+$input = file_get_contents('input.txt');
+
+if (!$input)
+    return 0;
+
+$best_elves = [
+    -1,
+    -2,
+    -3,
+];
+$elves = explode(PHP_EOL . PHP_EOL, $input);
+for ($i = 0; $i < count($elves); $i++) {
+    $calories = array_sum(explode(PHP_EOL, $elves[$i]));
+
+    for ($j = 0; $j < count($best_elves); $j++) {
+        if ($calories > $best_elves[$j]) {
+            moveElves($best_elves, $j, $calories);
+            print_r($best_elves);
+            break;
+        }
+    }
+
+}
+print_r($best_elves);
+echo ('Total: ' . array_sum($best_elves));
+
+function moveElves(&$best_elves, $start, $calories)
+{
+    array_pop($best_elves);
+    array_splice($best_elves, $start, 0, $calories);
+}
