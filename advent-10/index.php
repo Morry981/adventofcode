0b854f54e9fa793710eb5ba9a4c012b7d177fa14
Morry98 <89313187+Morry981@users.noreply.github.com>
2022-12-10 18:50:52 +0000

    Day 10 part 2

--- advent-10/index.php
+++ advent-10/index.php
@@ -3,6 +3,8 @@ $input = file_get_contents('input.txt');
 if (!$input)
     return 0;
 
+$sprite = [];
+
 $exec = [0];
 $total_sum = 0;
 $x = 1;
@@ -19,6 +21,24 @@ for ($cycle = 0; $cycle < count($exec); $cycle++) {
 
     if ($cycle === 20 || ($cycle - 20) % 40 === 0)
         $total_sum += $x * $cycle;
+
     $x += $exec[$cycle];
+
+    // Seconda parte
+    $sprite_line = floor($cycle / 40);
+    if ($sprite_line > 5) break;
+
+    $current_cycle = $cycle % 40;
+    $sprite[$sprite_line][] = ($current_cycle == $x - 1
+        || $current_cycle == $x
+        || $current_cycle == $x + 1
+    )
+        ? '#'
+        : '_';
 }
 echo ("Total sum: {$total_sum}");
+
+// Seconda parte
+echo (PHP_EOL);
+foreach ($sprite as $line)
+    echo (implode($line) . PHP_EOL);
